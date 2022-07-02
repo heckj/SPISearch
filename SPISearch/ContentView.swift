f7225af7d491867b16e51ad154f09be6e4ef22a1
Joe Heck <heckj@mac.com>
2022-07-02 15:09:22 -0700

    Initial Commit

---
+++ SPISearch/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SPISearch
+//
+//  Created by Joseph Heck on 7/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SPISearch/SPISearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPISearchApp.swift
+//  SPISearch
+//
+//  Created by Joseph Heck on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SPISearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

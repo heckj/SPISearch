ca4c0c0d597ac5ad913ce9b8b13d2b47e2c16a39
Joe Heck <heckj@mac.com>
2022-07-06 14:01:05 -0700

    more docs

--- SPISearch/Model/SearchMetrics.swift
+++ SPISearch/Model/SearchMetrics.swift
@@ -31,17 +31,38 @@ struct SearchMetrics {
         recall = SearchMetrics.calculateRecall(searchResult: searchResult, ranking: ranking)
         meanReciprocalRank = SearchMetrics.calculateMeanReciprocalRank(searchResult: searchResult, ranking: ranking)
     }
-
-    static func calculatePrecision(searchResult _: RecordedSearchResult, ranking _: RelevanceRecord) -> Double {
+    
+    /// Calculates the precision for a set of search results.
+    /// - Parameters:
+    ///   - searchResult: The search results to process.
+    ///   - ranking: The rankings used to evaluate the search results.
+    /// - Returns: A value between `0` and `1` that represents the precision of the search results.
+    ///
+    /// The precision of a set of search results is defined as ratio of the number of relevant documents compared to the total number of results retrieved.
+    static func calculatePrecision(searchResult: RecordedSearchResult, ranking: RelevanceRecord) -> Double {
         #warning("Implement calculatePrecision")
         return 0
     }
 
+    /// Calculates the recall for a set of search results.
+    /// - Parameters:
+    ///   - searchResult: The search results to process.
+    ///   - ranking: The rankings used to evaluate the search results.
+    /// - Returns: A value between `0` and `1` that represents the recall of the search results.
+    ///
+    /// The recall of a set of search results is defined as ratio of the number of relevant documents compared to the total number of relevant documents available.
     static func calculateRecall(searchResult _: RecordedSearchResult, ranking _: RelevanceRecord) -> Double {
         #warning("Implement calculateRecall")
         return 0
     }
 
+    /// Calculates the mean reciprocal rank for a set of search results.
+    /// - Parameters:
+    ///   - searchResult: The search results to process.
+    ///   - ranking: The rankings used to evaluate the search results.
+    /// - Returns: A value between `0` and `1` that represents the mean reciprocal rank of the search results.
+    ///
+    /// The mean reciprocal rank is a summed relevance rating for the search results, weighting earlier relevant results higher than later relevant results.
     static func calculateMeanReciprocalRank(searchResult _: RecordedSearchResult, ranking _: RelevanceRecord) -> Double {
         #warning("Implement calculateMeanReciprocalRank")
         return 0

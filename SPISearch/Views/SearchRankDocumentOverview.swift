3592d28d741960ad004a6efe0dee6b2a03da667f
Joe Heck <heckj@mac.com>
2022-07-09 15:10:49 -0700

    fixing duplicate nav structures in iOS

--- SPISearch/Views/SearchRankDocumentOverview.swift
+++ SPISearch/Views/SearchRankDocumentOverview.swift
@@ -12,6 +12,7 @@ struct SearchRankDocumentOverview: View {
     @State var selectedRanking: UUID? = nil
     @State var selectedSearchId: UUID? = nil
     var body: some View {
+#if os(macOS)
         NavigationView {
             VStack(alignment: .leading) { // primary nav view
                 Text("Relevance Rankings")
@@ -26,12 +27,6 @@ struct SearchRankDocumentOverview: View {
                                     .onTapGesture {
                                         selectedRanking = ranking.id
                                     }
-                                //                            Button {
-                                //                                print("deleting \(ranking.id)")
-                                //                            } label: {
-                                //                                Image(systemName: "minus.circle.fill").foregroundColor(.red)
-                                //                                    .font(.title)
-                                //                            }
                             }
                             .padding()
                             .background(selectedRanking == ranking.id ? .blue : .clear)
@@ -46,27 +41,60 @@ struct SearchRankDocumentOverview: View {
                         RecordedSearchResultView(result, relevancyValues: document.searchRanking.medianRelevancyRanking)
                     }
                 }
-                #if os(macOS)
                 .listStyle(.bordered)
-                #else
-                .listStyle(.plain)
-                .frame(maxHeight: 150)
-                #endif
-
-                Spacer()
             }
-            .padding()
-            .onAppear {
-                selectedRanking = document.searchRanking.relevanceSets.first?.id
-            }
-            if let selectedSearch = document.searchRanking.storedSearches.first(where: { $0.id == selectedSearchId }) {
-                RecordedSearchResultView(selectedSearch)
+            Spacer()
+        }
+        .padding()
+        .onAppear {
+            selectedRanking = document.searchRanking.relevanceSets.first?.id
+        }
+#else  // ^^ macOS layout, \/\/ iOS layout
+        VStack(alignment: .leading) { // primary nav view
+            Text("Relevance Rankings")
+                .font(.title)
+            if document.searchRanking.relevanceSets.isEmpty {
+                Text("No relevance rankings stored.")
             } else {
-                Text("Select a stored search to review")
+                HStack(alignment: .top) {
+                    ForEach(document.searchRanking.relevanceSets) { ranking in
+                        VStack {
+                            RelevanceSetSummaryView(ranking)
+                                .onTapGesture {
+                                    selectedRanking = ranking.id
+                                }
+                        }
+                        .padding()
+                        .background(selectedRanking == ranking.id ? .blue : .clear)
+                    }
+                }
             }
-        }.navigationTitle("Overview")
+            Text("Stored Searches for \(document.searchRanking.storedSearches.first?.searchTerms ?? "")")
+                .font(.title)
+            List(document.searchRanking.storedSearches, selection: $selectedSearchId) {
+                result in
+                NavigationLink("Search on \(result.recordedDate.formatted(date: .abbreviated, time: .omitted)) (\(result.host))") {
+                    RecordedSearchResultView(result, relevancyValues: document.searchRanking.medianRelevancyRanking)
+                }
+            }
+            .listStyle(.plain)
+            .frame(maxHeight: 150)
+            
+            Spacer()
+        }
+        .padding()
+        .onAppear {
+            selectedRanking = document.searchRanking.relevanceSets.first?.id
+        }
+#endif // ^^ iOS layout
     }
-
+// In a tab view under iOS, and under the existing DocumentBrowser nav setup, this shows up as an additional tab - not exactly what we're after...
+//        if let selectedSearch = document.searchRanking.storedSearches.first(where: { $0.id == selectedSearchId }) {
+//            RecordedSearchResultView(selectedSearch)
+//        } else {
+//            Text("Select a stored search to review")
+//        }
+    
     init(_ document: Binding<SearchRankDocument>) {
         _document = document
     }

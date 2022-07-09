9d417a0d7a60891984a1e50f6eee1a2d3a9f5391
Joe Heck <heckj@mac.com>
2022-07-09 11:03:14 -0700

    updating Testflight links in README, establishing two-pane navigation view from overview

--- SPISearch/Views/SearchRankDocumentOverview.swift
+++ SPISearch/Views/SearchRankDocumentOverview.swift
@@ -12,53 +12,59 @@ struct SearchRankDocumentOverview: View {
     @State var selectedRanking: UUID? = nil
     @State var selectedSearchId: UUID? = nil
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Relevance Rankings")
-                .font(.title)
-            if document.searchRanking.relevanceSets.isEmpty {
-                Text("No relevance rankings stored.")
-            } else {
-                HStack(alignment: .top) {
-                    ForEach(document.searchRanking.relevanceSets) { ranking in
-                        VStack {
-                            RelevanceSetSummaryView(ranking)
-                                .onTapGesture {
-                                    selectedRanking = ranking.id
-                                }
-//                            Button {
-//                                print("deleting \(ranking.id)")
-//                            } label: {
-//                                Image(systemName: "minus.circle.fill").foregroundColor(.red)
-//                                    .font(.title)
-//                            }
+        NavigationView {
+            VStack(alignment: .leading) { // primary nav view
+                Text("Relevance Rankings")
+                    .font(.title)
+                if document.searchRanking.relevanceSets.isEmpty {
+                    Text("No relevance rankings stored.")
+                } else {
+                    HStack(alignment: .top) {
+                        ForEach(document.searchRanking.relevanceSets) { ranking in
+                            VStack {
+                                RelevanceSetSummaryView(ranking)
+                                    .onTapGesture {
+                                        selectedRanking = ranking.id
+                                    }
+                                //                            Button {
+                                //                                print("deleting \(ranking.id)")
+                                //                            } label: {
+                                //                                Image(systemName: "minus.circle.fill").foregroundColor(.red)
+                                //                                    .font(.title)
+                                //                            }
+                            }
+                            .padding()
+                            .background(selectedRanking == ranking.id ? .blue : .clear)
                         }
-                        .padding()
-                        .background(selectedRanking == ranking.id ? .blue : .clear)
                     }
                 }
-            }
-            Text("Stored Searches for \(document.searchRanking.storedSearches.first?.searchTerms ?? "")")
-                .font(.title)
-            List(document.searchRanking.storedSearches, selection: $selectedSearchId) {
-                result in
-                SearchResultSetSummaryView(result)
-            }
-            #if os(macOS)
-            .listStyle(.bordered)
-            #else
-            .listStyle(.plain)
-            .frame(maxHeight: 150)
-            #endif
+                Text("Stored Searches for \(document.searchRanking.storedSearches.first?.searchTerms ?? "")")
+                    .font(.title)
+                List(document.searchRanking.storedSearches, selection: $selectedSearchId) {
+                    result in
+                    NavigationLink("Search on \(result.recordedDate.formatted(date: .abbreviated, time: .omitted)) (\(result.host))") {
+                        RecordedSearchResultView(result)
+                    }
+                }
+                #if os(macOS)
+                .listStyle(.bordered)
+                #else
+                .listStyle(.plain)
+                .frame(maxHeight: 150)
+                #endif
 
-            Spacer()
+                Spacer()
+            }
+            .padding()
+            .onAppear {
+                selectedRanking = document.searchRanking.relevanceSets.first?.id
+            }
             if let selectedSearch = document.searchRanking.storedSearches.first(where: { $0.id == selectedSearchId }) {
                 RecordedSearchResultView(selectedSearch)
+            } else {
+                Text("Select a stored search to review")
             }
-        }
-        .padding()
-        .onAppear {
-            selectedRanking = document.searchRanking.relevanceSets.first?.id
-        }
+        }.navigationTitle("Overview")
     }
 
     init(_ document: Binding<SearchRankDocument>) {

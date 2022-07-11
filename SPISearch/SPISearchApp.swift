f0a9c7c71c2293ff79c228fa5eaaff0b0edb244c
Joe Heck <heckj@mac.com>
2022-07-11 07:41:37 -0700

    CLI app for reporting searchrank files

--- SPISearch/SPISearchApp.swift
+++ SPISearch/SPISearchApp.swift
@@ -25,35 +25,6 @@ struct SPISearchApp: App {
         //        #endif
     }
 
-    init() {
-        #if os(macOS)
-            let info = ProcessInfo.processInfo
-            print("fullusername: \(info.fullUserName)")
-            print("userid: \(info.userName)")
-            print("arguments to app \(info.arguments)")
-            do {
-                let args = try SPISearchLaunchArguments.parse()
-                print("args: \(args)")
-
-                // TODO:
-                // - construct a URL for the file(s) from the options provided
-                // - use `startAccessingSecurityScopedResource()` to provide permissions to open that file
-                //  - ref: https://www.hackingwithswift.com/forums/swiftui/looking-for-help-how-to-select-and-open-an-existing-data-file-with-a-document-browser/3953
-                // - read the file & deserialize into a searchRank model
-                // - print the permissions from that model
-                // - call `stopAccessingSecurityScopedResource()` to wrap up after file's read
-                
-                // App store apps suggest to NEVER do this:
-                // exit(0)
-
-            } catch {
-                print("Error: Could not parse arguments")
-                print(CommandLine.arguments.dropFirst().joined(separator: " "))
-                print(SPISearchLaunchArguments.helpMessage())
-            }
-        #endif
-    }
-
     /// The key used for storing the identity of the reviwer in AppStorage (UserDefaults).
     static var reviewerKey: String = "reviewer"
 }

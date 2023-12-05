63a66aed336b7b3494bc75f9b8a8f8216c8aedd3
Joe Heck <heckj@mac.com>
2023-12-05 15:53:25 -0800

    cleanup, scheme data (argument for CLI) added

--- CaptureSearches/CaptureSearches.swift
+++ CaptureSearches/CaptureSearches.swift
@@ -14,7 +14,7 @@ struct CaptureSearches: AsyncParsableCommand {
 
     @Option(help: "File name to write with the captured searches.")
     var encodedsearchcollection: String = "searchcollection.txt"
-    
+
     mutating func run() async throws {
         let jsonEncoder = JSONEncoder()
         jsonEncoder.dateEncodingStrategy = .iso8601
@@ -28,47 +28,46 @@ struct CaptureSearches: AsyncParsableCommand {
         print("Reading queries from \(fileURLtoRead.absoluteString)")
         let fileURLtoWrite = URL(fileURLWithPath: encodedsearchcollection)
 
-            let outputFilePath = fileURLtoWrite.path()
-            print("Writing to file: \(outputFilePath)")
-            // Check for file presence and create it if it does not exist
-            let fileManager = FileManager.default
+        let outputFilePath = fileURLtoWrite.path()
+        print("Writing to file: \(fileURLtoWrite.absoluteString)")
+        // Check for file presence and create it if it does not exist
+        let fileManager = FileManager.default
 
-            // If file exists, remove it
-            if fileManager.fileExists(atPath: outputFilePath) {
-                try fileManager.removeItem(atPath: outputFilePath)
-            }
-
-            // Create file and open it for writing
-            fileManager.createFile(atPath: outputFilePath, contents: Data(" ".utf8), attributes: nil)
-            if let fileHandle = FileHandle(forWritingAtPath: outputFilePath),
-               let newlineAsData = "\n".data(using: .utf8)
-            {
-                // Write data
-                fileHandle.write("Test line 1\n".data(using: .utf8)!)
-
-                do {
-                    let (bytes, response) = try await URLSession.shared.bytes(from: fileURLtoRead)
-                    print(" - processing \(response.expectedContentLength) bytes:")
-                    for try await line in bytes.lines {
-                        // do something...
-                        print("Requesting search results for '\(line)'")
-                        let searchresult = try await makeASearchResult(terms: line,
-                                                                       from: host,
-                                                                       apiToken: apiToken)
-                        dump(searchresult)
-                        let encoded = try jsonEncoder.encode(searchresult)
-                        fileHandle.write(encoded)
-                        fileHandle.write(newlineAsData)
-                    }
-                } catch {
-                    print("Error: \(error.localizedDescription)")
-                }
-
-                // Close file
-                fileHandle.closeFile()
-            } else {
-                fatalError("nil file handle for output file")
-            }
+        // If file exists, remove it
+        if fileManager.fileExists(atPath: outputFilePath) {
+            try fileManager.removeItem(atPath: outputFilePath)
         }
-    
+
+        // Create file and open it for writing
+        fileManager.createFile(atPath: outputFilePath, contents: Data(" ".utf8), attributes: nil)
+        if let fileHandle = FileHandle(forWritingAtPath: outputFilePath),
+           let newlineAsData = "\n".data(using: .utf8)
+        {
+            // Write data
+            fileHandle.write("Test line 1\n".data(using: .utf8)!)
+
+            do {
+                let (bytes, response) = try await URLSession.shared.bytes(from: fileURLtoRead)
+                print(" - processing \(response.expectedContentLength) bytes:")
+                for try await line in bytes.lines {
+                    // do something...
+                    print("Requesting search results for '\(line)'")
+                    let searchresult = try await makeASearchResult(terms: line,
+                                                                   from: host,
+                                                                   apiToken: apiToken)
+                    dump(searchresult)
+                    let encoded = try jsonEncoder.encode(searchresult)
+                    fileHandle.write(encoded)
+                    fileHandle.write(newlineAsData)
+                }
+            } catch {
+                print("Error: \(error.localizedDescription)")
+            }
+
+            // Close file
+            fileHandle.closeFile()
+        } else {
+            fatalError("nil file handle for output file")
+        }
+    }
 }

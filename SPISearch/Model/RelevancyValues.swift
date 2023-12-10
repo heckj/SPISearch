702d7134e212cc12d9d5f1dd6e17bf22008b4d46
Joe Heck <heckj@mac.com>
2023-12-10 10:06:52 -0800

    fixing relevancy computations - need to add a means to get values from rankings stored to use it, and finish 'complete?'

---
+++ SPISearch/Model/RelevancyValues.swift
@@ -0,0 +1,9 @@
+import SPISearchResult
+
+/// A type that holds the average relevancy values computed from the stored rankings in the document.
+struct RelevancyValues {
+    /// The query terms used for the related searches
+    let query_terms: String
+    /// A combined set of relevance choices for the packages from all searches matching the query terms.
+    var relevanceValue: [SearchResult.Package.PackageId: Double]
+}

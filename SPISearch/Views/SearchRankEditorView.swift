4c872f82cb9f0b27327ebf1ee29a9746cf14302d
Joe Heck <heckj@mac.com>
2024-02-01 11:18:17 -0800

    enabling a UITest with accessibility auditing

--- SPISearch/Views/SearchRankEditorView.swift
+++ SPISearch/Views/SearchRankEditorView.swift
@@ -6,22 +6,20 @@ struct SearchRankEditorView: View {
     @Binding var document: SearchRankDocument
 
     var body: some View {
-        VStack {
-            #if os(macOS)
-                NavigationSplitView {
-                    SearchRankDocumentOverview($document)
-                } detail: {
-                    Text("Select ranking or search in the sidebar to view.")
-                }
-            #else
+        #if os(macOS)
+            NavigationSplitView {
                 SearchRankDocumentOverview($document)
-            #endif
-        }
+            } detail: {
+                Text("Select ranking or search in the sidebar to view.")
+            }
+            .frame(idealWidth: 500, maxWidth: .infinity, idealHeight: 300, maxHeight: 1500)
         // Applied to the top level view in a macOS App, this controls both the initial size
         // of the window that appears and the maximum size to which it can be expanded.
-        .frame(idealWidth: 500, maxWidth: .infinity, idealHeight: 300, maxHeight: 1500)
         // in macOS 13+ this can be replaced with the .defaultSize(width: 1000, height: 650)
         // modifier on the enclosing scene to create a default size experience.
+        #else
+            SearchRankDocumentOverview($document)
+        #endif
     }
 }
 
